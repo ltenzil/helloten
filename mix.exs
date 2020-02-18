c0675be0beec908efdbc12971261654aa41207e7
ltenzil <ltenzil@gmail.com>
2020-02-18 12:11:01 +0530

    Publish details added

--- mix.exs
+++ mix.exs
@@ -9,7 +9,9 @@ defmodule Hello.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
-      licenses: 
+      package: package(),
+      name: "Helloten",
+      source_url: "https://github.com/ltenzil/helloten"
     ]
   end
 
@@ -35,12 +37,12 @@ defmodule Hello.MixProject do
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
-      name: "postgrex",
+      name: "helloten",
       # These are the default files included in the package
       files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+                license*),
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
+      links: %{"GitHub" => "https://github.com/ltenzil/helloten"}
     ]
   end
 end
